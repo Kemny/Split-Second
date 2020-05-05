b8de6415ce6a35a2d63c63b4b36ea6514358f235
Reetro <thegamerboy7@gmail.com>
2020-05-05 10:47:15 -0600

    Created base classes for all AI characters

---
+++ Source/SplitSecond/AI/AI_ShooterBase.h
@@ -0,0 +1,17 @@
+// This project falls under CC-BY-SA lisence
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "../AI/Super_AI_Character.h"
+#include "AI_ShooterBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SPLITSECOND_API AAI_ShooterBase : public ASuper_AI_Character
+{
+	GENERATED_BODY()
+	
+};
