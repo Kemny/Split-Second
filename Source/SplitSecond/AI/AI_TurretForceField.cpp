8266028a7bdf6cd7d596a8ac0c28b593eb070447
Reetro <thegamerboy7@gmail.com>
2020-05-10 07:36:04 -0600

    started to work on the tturret shield

---
+++ Source/SplitSecond/AI/AI_TurretForceField.cpp
@@ -0,0 +1,27 @@
+// This project falls under CC-BY-SA lisence
+
+
+#include "AI_TurretForceField.h"
+
+// Sets default values
+AAI_TurretForceField::AAI_TurretForceField()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AAI_TurretForceField::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AAI_TurretForceField::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
