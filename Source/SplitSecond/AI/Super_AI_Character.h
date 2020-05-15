6dd6b031ab533b0171656672ebf50ac9fc0d3e18
Reetro <thegamerboy7@gmail.com>
2020-05-15 12:31:44 -0600

    Turret shield fixes

--- Source/SplitSecond/AI/Super_AI_Character.h
+++ Source/SplitSecond/AI/Super_AI_Character.h
@@ -17,42 +17,49 @@ public:
 	// Sets default values for this character's properties
 	ASuper_AI_Character();
 
-  UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
-  class UHealthComponent* HealthComponent;
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+    class UHealthComponent* HealthComponent;
 
-  UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
-  class UBoxComponent* TraceComp;
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+    class UBoxComponent* TraceComp;
 
 public:	
+    /* Gun to give AI */
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Gun Settings")
+    TSubclassOf<ASuper_Gun> AIGunClass;
 
-  /* Gun to give AI */
-  UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Gun Settings")
-  TSubclassOf<ASuper_Gun> AIGunClass;
+    /* The Behavior Tree the AI will use */
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "AI Settings")
+    class UBehaviorTree* BehaviorTreeToUse;
 
-  /* The Behavior Tree the AI will use */
-  UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "AI Settings")
-  class UBehaviorTree* BehaviorTreeToUse;
+    /* the maximum amount of distance the AI can be from it's target */
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "AI Settings")
+    float MaxTargetDistance;
 
-  /* the maximum amount of distance the AI can be from it's target */
-  UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "AI Settings")
-  float MaxTargetDistance;
+    /* whether or not the AI is able to rotate to player */
+    UPROPERTY(BlueprintReadWrite)
+    bool bAbleToRotate;
 
-  /* Enemies gun fire delay */
-  UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "AI Settings")
-  float GunFireDelay;
+	/* whether or not the AI is able to shoot there current weapon */
+	UPROPERTY(BlueprintReadWrite)
+    bool bCanShoot;
 
-  /* AI's current gun */
-  UPROPERTY(BlueprintReadWrite, Category = "Gun Vars")
-  ASuper_Gun* CurrentGun;
+    /* Enemies gun fire delay */
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "AI Settings")
+    float GunFireDelay;
 
-  /* Fires the currently equipped gun */
-  UFUNCTION(BlueprintCallable, Category = "Gun Functions")
-  void FireGun();
+    /* AI's current gun */
+    UPROPERTY(BlueprintReadWrite, Category = "Gun Vars")
+    ASuper_Gun* CurrentGun;
+
+    /* Fires the currently equipped gun */
+    UFUNCTION(BlueprintCallable, Category = "Gun Functions")
+    void FireGun();
 
 public:
 
-  UFUNCTION(BlueprintCallable, Category = "Health")
-  FORCEINLINE class UHealthComponent* GetHealthComponent() const { return HealthComponent; }
+    UFUNCTION(BlueprintCallable, Category = "Health")
+    FORCEINLINE class UHealthComponent* GetHealthComponent() const { return HealthComponent; }
 
     void Highlight();
     void GetSlowed(float SlowTime, float SlowAmmount);
