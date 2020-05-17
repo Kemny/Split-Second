d5f61af8fc7a78230631260f87e1386471dfbd50
Kemny <l.krchov@gmail.com>
2020-05-17 14:25:38 +0200

    Added code For Arena Objectives, BUG: when trying to destroy old enemies when switching level, the game freezes

---
+++ Source/SplitSecond/Libraries/SplitSecondArenas.h
@@ -0,0 +1,17 @@
+// This project falls under CC-BY-SA lisence
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "SplitSecondArenas.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SPLITSECOND_API USplitSecondArenas : public UObject
+{
+	GENERATED_BODY()
+	
+};
