45fee72e659ae9b098d51496ffa4f27471c2bcc7
Reetro <thegamerboy7@gmail.com>
2020-05-18 15:36:07 -0600

    Started work on boss class

---
+++ Source/SplitSecond/AI/Super_Boss.h
@@ -0,0 +1,17 @@
+// This project falls under CC-BY-SA lisence
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AI/Super_AI_Character.h"
+#include "Super_Boss.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SPLITSECOND_API ASuper_Boss : public ASuper_AI_Character
+{
+	GENERATED_BODY()
+	
+};
